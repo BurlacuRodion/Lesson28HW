f167128ab4f693c60763eb7933452599b82ff71b
Burlacu Rodion <burlacurodionmd@gmail.com>
2024-05-06 14:27:10 +0300

    add last ex

--- index.js
+++ index.js
@@ -8,12 +8,36 @@
 // }
 // getData()
 
-// Array.prototype.myFunc = function(callback) {
+// Array.prototype.mySome = function(callback) {
 //     return this.reduce((acc, curr, idx, arr)=>{
 //         if(acc) return acc;
 //         return callback(curr, idx, arr)
 //     },false)
 // }
-// console.log([4,5,6,7].myFunc(el => el > 7)) // false
+// console.log([4,5,6,7].mySome(el => el > 7)) // false
 
+// Array.prototype.myEvery = function(callback) {
+//     return this.reduce((acc, curr, idx, arr)=>{
+//         if(!acc) return acc;
+//         return callback(curr, idx, arr)
+//     }, true)
+// }
+// console.log([1,2,3].myEvery( item => item < 4)) //true
+
+Array.prototype.mySlice = function(first, last) {
+
+    const startIdx = first ? first < 0 ? this.length - 1 - Math.abs(first) : first : 0;
+    const lastIdx = last ? last < 0 ? this.length - 1 - Math.abs(last) : last : 0;
+
+    return this.reduce((acc, curr, idx) => {
+        if(idx >= startIdx && idx <= lastIdx) {
+            acc.push(curr)
+            return acc;
+        }
+        return acc;
+    },[])
+}
+
+console.log([1,2,3,4,5,6,7].slice(3,7))
+console.log([1,2,3,4,5,6,7].mySlice(3,7))
 
